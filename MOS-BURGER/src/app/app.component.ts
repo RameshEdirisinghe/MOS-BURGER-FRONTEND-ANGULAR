52da9194efb7221ae715261c50f2ec2922c25745
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-08 15:21:53 +0530

    login page design roteer path done

--- MOS-BURGER/src/app/app.component.ts
+++ MOS-BURGER/src/app/app.component.ts
@@ -3,19 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { ProductManagementComponent } from './page/dashboard/product/product.component';
-import { OrderManagementComponent } from "./page/dashboard/order/order.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductManagementComponent, OrderManagementComponent],
-  template: `
-    <!-- <app-product-management></app-product-management> -->
-    <app-order-management></app-order-management>
-    
-  `
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: `./app.component.html` 
+  
 })
 export class AppComponent implements OnInit {
   title = 'Mos Burger';
--- MOS-BURGER/src/app/app.routes.ts
+++ MOS-BURGER/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { ProductManagementComponent } from './page/dashboard/product/product.component';
+import { OrderManagementComponent } from './page/dashboard/order/order.component';
+import { ReportComponent } from './page/dashboard/report/report.component';
+import { PlaceorderComponent } from './page/placeorder/placeorder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: LoginComponent
+    },
+    {
+        path:"login",
+        component: LoginComponent
+    },
+    {
+        path:"dashboard",
+        component: DashboardComponent,
+
+        children: [
+            {
+                path: "product",
+                component: ProductManagementComponent
+            },
+            {
+                path: "order",
+                component: OrderManagementComponent
+            },
+            {
+                path:"",
+                component: ReportComponent
+            }
+        ]
+    },
+    {
+        path:"placeholder",
+        component: PlaceorderComponent
+    }
+];
